ba380370171ec05828e086ecb118ed2a6b301efa
Your NameKhansaaObeidat <khansaaobeidat48@gmail.com>
2024-10-11 16:51:06 +0300

    00

--- PatientProject/Program.cs
+++ PatientProject/Program.cs
@@ -2,19 +2,30 @@ using Microsoft.EntityFrameworkCore;
 using PatientProject.Data;
 using PatientProject.Mappings;
 using PatientProject.Repository;
-using System;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("constr")));
-builder.Services.AddScoped<IPatientRepository,SQLPatientRepository>();
+builder.Services.AddScoped<IPatientRepository, SQLPatientRepository>();
 builder.Services.AddAutoMapper(typeof(AutoMapperProfiles));
+
+// Add CORS policy
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowSpecificOrigin", builder =>
+    {
+        builder.WithOrigins("https://localhost:7285")  // Replace with your front-end URL
+               .AllowAnyHeader()
+               .AllowAnyMethod()
+               .AllowCredentials();  // If needed, for cookies/auth
+    });
+});
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -25,6 +36,9 @@ if (app.Environment.IsDevelopment())
 }
 
 app.UseHttpsRedirection();
+
+// Enable CORS
+app.UseCors("AllowSpecificOrigin");
 
 app.UseAuthorization();
 
---
+++ PatientProjectUI/Controllers/PatientController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PatientProjectUI.Models.Dto;
+
+namespace PatientProjectUI.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult AddPatient()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult AddPatient(AddPationtDto addPatient)
+        {
+            return View();
+        }
+    }
+}
